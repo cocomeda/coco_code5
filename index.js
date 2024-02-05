d3edc7784716d831b6e77e1548c29029fd430e5e
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-02-05 15:36:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ function textToUnicode(text) {
 
 
 function sendQRValueToAPI_2(qrValue) { // GETリクエスト
-  var apiUrl = 'https://script.google.com/macros/s/AKfycbxni9yIxdKVlCTXtMcU2AJnn7eiE43sK4gb9-WCdhhLiVdpUCHRl8sbFyZBQFjTlGKVeg/exec'//+"?qrValue="+qrValue; //GET
+  var apiUrl = 'https://script.google.com/macros/s/AKfycbwkOZwGWOARwAWMrViR2Zq7fZlfLXNFAT3SD9FMG_2ibv2tiz9Jfme2YuWGKFc283pdqw/exec'//+"?qrValue="+qrValue; //GET
 	
     // GETリクエストの場合、クエリパラメータとしてデータを渡す
     apiUrl += '?qrValue=' + encodeURIComponent(qrValue);//
