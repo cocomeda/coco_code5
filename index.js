e1c3769a04c4a5362e1324dcece880adca23fdf3
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-02-06 12:36:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function textToUnicode(text) {
 
 
 function sendQRValueToAPI_2(qrValue) { // GETリクエスト
- var apiUrl = 'https://script.google.com/macros/s/AKfycbw_yXpHu_Wi1Ff0uubenXVgKzSccN7gA-M8kkKgpyq98xKXx_7iTSyG2WhjwaylLzFARw/exec'//+"?qrValue="+qrValue; //GET
+ var apiUrl = 'https://script.google.com/macros/s/AKfycbyq4MSw9UilpU-BMuyXusJz-szFwVENhlrNL4-GG1R5RuVjmX44LD_GadRQKwUnSrEoWA/exec'//+"?qrValue="+qrValue; //GET
 // var apiUrl = 'https://script.google.com/macros/s/AKfycbziLTHejlwFYPaHUWZ0QELwBoyESfJdH91qbSn3mQVQrCOxcX4T2wFDdGrOmDLh7-gZvA/exec'//+"?qrValue="+qrValue; //GET
 	
         // GETリクエストの場合、クエリパラメータとしてデータを渡す
