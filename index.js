1ee3df4d90343c46f4916ae63fd66a99a9b76f9f
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-02-15 16:21:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function textToUnicode(text) {
  
 function sendQRValueToAPI_2(qrValue) { // GETリクエスト
 	//var apiUrl = 'https://script.google.com/macros/s/AKfycbz6RnNKVM-DoeS1ls6viVFfqpNN5sNF2KNNsCUQAqaLSSkk4RK3TryzQd5HbntZdm_m2w/exec'//+"?qrValue="+qrValue; //GET
- var apiUrl = 'https://script.google.com/macros/s/AKfycbzA2d2pg_Q-pRtB4WQ3VOFo4SDWlhTRdzSIOOYQlFUHOXV7UVt1Xcj5VbuN9YT6TR1pZQ/exec'//+"?qrValue="+qrValue; //GET
+ var apiUrl = 'https://script.google.com/macros/s/AKfycbyhwbvwUuv3ZrdDsnZhtEZ9AlDeYdreuDDZMjm7XAUfpSKwMSg4biSlOWbs0tLtDS27ew/exec'//+"?qrValue="+qrValue; //GET
 
 // var apiUrl = 'https://script.google.com/macros/s/AKfycbziLTHejlwFYPaHUWZ0QELwBoyESfJdH91qbSn3mQVQrCOxcX4T2wFDdGrOmDLh7-gZvA/exec'//+"?qrValue="+qrValue; //GET
 	//var apiUrl = 'https://script.google.com/macros/s/AKfycbwTyd4Qe7yR73Y9Y2YpkKTTvdfptM5TutZQqgobSWoVCU4lDVbGsLDdvgagF-NccZ7PaQ/exec'//+"?qrValue="+qrValue; //GET
