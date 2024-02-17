1af45bd00a57cb9b3c35984cd4f28402c34e4a88
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-02-17 19:43:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,5 @@
 
 
-function pre_a(){
-        // LIFFの初期化
-        liff.init({ liffId: '2001269046-RZ90vdYB' }, () => {
-            if (liff.isLoggedIn()) {
-                // ユーザーがログインしている場合
-      openQRCodeReader()
-            } else {
-                // ログインが必要な場合、ログインページを表示
-                liff.login();
-            }
-        });
-
-}
-
-
-
-        // IDトークンをGASに送信する関数
-        function sendIdTokenToGAS() {
-            const idToken = liff.getIDToken();
-
-            // ローディング画像表示
-            $('#loading').show();
-
-            // IDトークンをGASに送信
-            $.ajax({
-                url: 'https://script.google.com/macros/s/AKfycbzVJ4-JcdVxv0wWy3t9ZNSOIVHL1tJHQk-An64tuqZeRj9C6gpzr5n9KDrF6UBuIG3b/exec',
-                
-                type: 'POST',
-                data: { idToken: idToken },
-                success: function (response) {
-                    // 成功時の処理
-                    console.log(response);
-                    displayMessage(response); // レスポンスメッセージを表示
-                    displayData(response);
-                },
-                error: function (error) {
-                    // エラー時の処理
-                    console.error(error);
-                    alert('Failed to send ID Token to GAS.');
-                },
-                complete: function () {
-                    // 通信完了時の処理（ローディング画像を非表示にし、コンテンツを表示）
-                    $('#loading').hide();
-                    $('#content').show();
-                }
-            });
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -130,9 +64,9 @@ function openQRCodeReader() {
 
                    // let aaa = "qr_data:" + String(cc);
                     let aaa = String(cc);
-sendIdTokenToGAS();
+
 			
-                   // sendText(aaa);
+                    sendText(aaa);
 
 			
                 } catch (err) {
