7bf42f0446e1a81ccd33084674fc849176920b31
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-02-17 19:34:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
+
+
+
+        // LIFFの初期化
+        liff.init({ liffId: '2001905227-7dKy6ERe' }, () => {
+            if (liff.isLoggedIn()) {
+                // ユーザーがログインしている場合
+      openQRCodeReader()
+            } else {
+                // ログインが必要な場合、ログインページを表示
+                liff.login();
+            }
+        });
+
+        // IDトークンをGASに送信する関数
+        function sendIdTokenToGAS() {
+            const idToken = liff.getIDToken();
+
+            // ローディング画像表示
+            $('#loading').show();
+
+            // IDトークンをGASに送信
+            $.ajax({
+                url: 'https://script.google.com/macros/s/AKfycbz-ffWFPc36O-ptmFqijL10vbctVuUm1i_Yv3KgjKQotJnXtGlITC4klGh0cJ7RaS28Ww/exec',
+                
+                type: 'POST',
+                data: { idToken: idToken },
+                success: function (response) {
+                    // 成功時の処理
+                    console.log(response);
+                    displayMessage(response); // レスポンスメッセージを表示
+                    displayData(response);
+                },
+                error: function (error) {
+                    // エラー時の処理
+                    console.error(error);
+                    alert('Failed to send ID Token to GAS.');
+                },
+                complete: function () {
+                    // 通信完了時の処理（ローディング画像を非表示にし、コンテンツを表示）
+                    $('#loading').hide();
+                    $('#content').show();
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var params = (new URL(document.location)).searchParams;
 var key = params.get('key');
 
@@ -58,7 +126,11 @@ function openQRCodeReader() {
 
                    // let aaa = "qr_data:" + String(cc);
                     let aaa = String(cc);
-                    sendText(aaa);
+sendIdTokenToGAS();
+			
+                   // sendText(aaa);
+
+			
                 } catch (err) {
                     console.error('Error sending QR value to API:', err);
                 }
